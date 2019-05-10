f56f18faca9b876fa4267b22f3acc8d7154a382d
hxf <Erazer_Control@163.com>
2019-05-10 21:40:11 +0800

    主页电影列表

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,48 +4,24 @@ const app = getApp()
 
 Page({
   data: {
-    mode:1
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    mode:1,
+    movieList:[]
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    var that=this;
+    wx.request({
+      url:'https://m.douban.com/rexxar/api/v2/subject_collection/movie_showing/items',
+      data:{
+        count:7
+      },
+      success:function(data){
+        console.log(data);
+        that.setData({
+          movieList:data.data.subject_collection_items
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      
     })
+
   }
 })
